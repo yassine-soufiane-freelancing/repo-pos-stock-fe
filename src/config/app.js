02dec1abec664df5bff74249401e09b39cb63136
El kaabaoui Soufiane <100730344+soufianeElkaabaoui@users.noreply.github.com>
2026-03-31 15:06:25 +0100

    Update frontend and backend URLs for production

--- src/config/app.js
+++ src/config/app.js
@@ -1,6 +1,6 @@
 
-export const frontendUrl = 'http://localhost:8080/'
+export const frontendUrl = 'https://restau-pos.webyms.com/'
 // export const frontendUrl = 'http://192.168.147.46:8080/'
-export const backendUrl = 'http://localhost:8000/'
+export const backendUrl = 'https://be.restau-pos.webyms.com/'
 
 export const currency = "DH";
